8e3a23a3eead7cdcf8299eeb36adbdfad72a2438
derp-derp-derp <derpes@protonmail.com>
2023-07-11 00:06:53 -0400

    Implement new absolute path env vars in .find reverse lookup PHP endpoint script

--- public/endpoints/find-reverse-lookup.php
+++ public/endpoints/find-reverse-lookup.php
@@ -17,7 +17,8 @@ if(isset($_GET['flow_address']))
         );
     }
     
-    $find_lookup_cadence = shell_exec($env['FLOW_CMD_PATH'] ." --network='mainnet' scripts -o 'json' execute ../../cadence/find-name-reverse-lookup.cdc ". $flow_address);
+    $cmd = $env['FLOW_CMD_ABS_PATH'] ." --config-path '". $env['FLOW_SCRIPTS_ABS_PATH'] ."/flow.json' --network='mainnet' -o 'json' --log='none' scripts execute '". $env['FLOW_SCRIPTS_ABS_PATH'] ."/find-name-reverse-lookup.cdc' ". $flow_address;
+
     $find_lookup_json = json_decode($find_lookup_cadence, true);
     
     if(!is_null($find_lookup_json['value']['value']))
