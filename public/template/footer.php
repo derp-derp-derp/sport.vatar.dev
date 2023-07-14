9698f07d893f9097fab29a7863c69a902fffc35b
derp-derp-derp <derpes@protonmail.com>
2023-07-14 15:20:31 -0400

    Mobile rendering improvements for global footer

--- public/template/footer.php
+++ public/template/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    This website was built for fun by a fan of <a href="https://sportvatar.com" target="_blank" class="text_link_bright">Sportvatar</a>.<br class="mobile-only">The data comes with no guarantee of accuracy.
+    This site was built for fun by a <a href="https://sportvatar.com" target="_blank" class="text_link_bright">Sportvatar</a> fan.<br class="mobile-only">The data comes with no guarantee of accuracy.
 </footer>
 
 </body>
